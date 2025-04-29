0fd5068117b7e9161c3f3415be7bda3a01434dd7
Alexandre de Sousa <alexandre.qsp@gmail.com>
2025-04-29 13:41:03 -0300

    second commit

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Home from "./Home.tsx";
-import Form from "./Form.tsx";
+import Form2 from "./Form2.tsx";
 
 function Router(){
 
@@ -9,7 +9,7 @@ function Router(){
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/cadastro" element={<Form />} />
+                <Route path="/cadastro" element={<Form2 />} />
             </Routes>
         </BrowserRouter>
     )
